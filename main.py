17efedfe8b73eb1db12555122d349ba8544e66c8
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-03 11:14:35 +0300

    Update main.py
    All comments added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#подключение файла make.py
 import make
-print("where can I find your result.json file?")
+print("Введите путь к файлу result.json. Если он лежит в корневой папке программы(рядом с main.py) просто нажмите Enter")
 json_adr = input()
-print("OK, where to put the image?")
+print("Введите путь, по которому сохранится изображение. Чтобы сохранить корневой папке программы(рядом с main.py) просто нажмите Enter")
 res_adr = input()
+#вызов функции генерации картинки
 make.make(json_adr, res_adr)
