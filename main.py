c04918bdfb92bb9178dc828bbde99f4eeec5517f
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-03 15:14:50 +0300

    Update main.py
    Console update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #подключение файла make.py
 import make
-print("Введите путь к файлу result.json. Если он лежит в корневой папке программы(рядом с main.py) просто нажмите Enter")
-json_adr = input()
-print("Введите путь, по которому сохранится изображение. Чтобы сохранить корневой папке программы(рядом с main.py) просто нажмите Enter")
-res_adr = input()
+#print("Введите путь к файлу result.json. Если он лежит в корневой папке программы(рядом с main.py) просто нажмите Enter")
+#json_adr = input()
+#print("Введите путь, по которому сохранится изображение. Чтобы сохранить корневой папке программы(рядом с main.py) просто нажмите Enter")
+#res_adr = input()
 #вызов функции генерации картинки
-make.make(json_adr, res_adr)
+#make.make(json_adr, res_adr)
+print('Starting...')
+make.make('result.json', 'freq_words.png')
+print('Done.')
