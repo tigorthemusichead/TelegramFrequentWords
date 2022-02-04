12e702379ccec917cf00fa097f8a47481eadfb0a
tigorthemusichead <92551370+tigorthemusichead@users.noreply.github.com>
2022-02-05 02:12:09 +0300

    Update make.py
    KeyError fixed. Thanx to Timur

--- make.py
+++ make.py
@@ -18,13 +18,14 @@ def make(json_adr, res_adr):
         data = json.load(f)
         #перебор всех сообщений
         for i in data['messages']:
-            if type(i['text']) == str:     
-                word = i['text'].lower()
-                #исключение знаков пунктуации
-                for c in punct:
-                    word = i['text'].replace(c, '').lower()
-                #добавление слова в строку со всей перепиской
-                data_string += word + ' '
+            if i.get('text') is not None:
+                if type(i['text']) == str:
+                    word = i['text'].lower()
+                    #исключение знаков пунктуации
+                    for c in punct:
+                        word = i['text'].replace(c, '').lower()
+                    #добавление слова в строку со всей перепиской
+                    data_string += word + ' '
         f.close()
     #настройка картинки
     wc = WordCloud(
@@ -35,7 +36,7 @@ def make(json_adr, res_adr):
         height=1000,
         #ширина картинки (можно менять)
         width=1000,
-        #количество слов на картинке (можно менять, чем больше слов, тем дольше работает программа) 
+        #количество слов на картинке (можно менять, чем больше слов, тем дольше работает программа)
         #python anywhere справляется только с 80ю, компьютер легко обрабатывает больше 1000
         max_words=80
     )
@@ -43,4 +44,3 @@ def make(json_adr, res_adr):
     wc.generate(data_string)
     #сохранение картинки
     wc.to_file(res_adr + 'word_cloud.png')
-
